e701050129769cfa415ad185621ecb49c6eebcaa
NHanzhuha <NHanzhuha@healthprecision.com>
2024-05-02 18:05:58 -0600

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   },
   openGraph: {
     description: 'Here all friends of Juliet could leave their data for party invitation',
-    images: `./juliet-photo.png`,
+    images: `https://firebasestorage.googleapis.com/v0/b/dad-birthday-party.appspot.com/o/party-system%2Fd48a1017-8d92-4aa1-b8d2-49021fefa6d1%204%20(1).png?alt=media&token=2ad83ebf-142b-4d11-bf4e-b5e8cb420fa2`,
     locale: 'en_US',
     type: 'website',
     siteName: 'Juliet RSVP'
